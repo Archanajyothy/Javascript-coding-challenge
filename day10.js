63b6c54181dfed5644c76c63c0e448804f77579b
archanajyothy <archanajyothy@gmail.com>
2023-05-29 10:27:25 +0530

    Qusetion added

--- day10.js
+++ day10.js
@@ -1,3 +1,18 @@
+/* 
+Example1
+Input: arr1 = [4,1,3] , arr2 = [5,7]
+Output: 15
+Explanation: The number 15 contains the digit 1 from arr1 
+and the digit 5 from arr2.
+It can be proven that 15 is the smallest number we can have.
+
+Example2
+Input: arr1=[3,5,2,6], arr2=[3,1,7]
+Output:3
+Explanation: The number 3 contains the 
+digit 3 which exists in both arrays.
+*/ 
+
 function smallest(arr1,arr2){
     let arr3 = []
     for(let i =0;i<arr1.length; i++){
