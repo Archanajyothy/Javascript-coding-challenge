ac4b8aaa96dec6aac44888ffe8588e8c87205922
archanajyothy <archanajyothy@gmail.com>
2023-06-02 10:32:34 +0530

    javascript daily

--- day13.js
+++ day13.js
@@ -1,3 +1,22 @@
+/*
+Example 1
+
+Input: n = 4, time = 5
+Output: 2
+Explanation: People pass the pillow in the following way:
+1 -> 2 -> 3 -> 4 -> 3 -> 2.
+After five seconds, the pillow is given to the 2nd person.  
+
+Example 2
+
+Input: n = 3, time =2 
+Output: 3
+Explanation: People pass the pillow in the following way:
+1 -> 2 -> 3.
+After two seconds, the pillow is given to the 3rd person. 
+*/
+
+
 function pass(n,t){
     let arr = [], num =0
     if(t<n){
