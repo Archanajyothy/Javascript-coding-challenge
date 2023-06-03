c9e10df589ce27412b12cbaf9b28d2ab15b0f10b
archanajyothy <archanajyothy@gmail.com>
2023-06-03 23:29:25 +0530

    Question added

--- day14.js
+++ day14.js
@@ -1,3 +1,20 @@
+/* 
+Example 1
+Input: distance =[1,2,3,4],start = 0, destination = 1
+Output: 1
+Explanation: Distance between 0 and 1 is 1 or 9, minimum is 1.
+
+Example 2
+Input: distance =[1,2,3,4],start = 0, destination = 2
+Output: 3
+Explanation: Distance between 0 and 2 is 2 or 3, minimum is 3.
+
+Example 3
+Input: distance =[1,2,3,4],start = 0, destination = 3
+Output: 4
+Explanation: Distance between 0 and 3 is 6 or 4, minimum is 4.
+*/ 
+
 function minDistance(arr,start,dest){
     let i = 0
     let j = 4
