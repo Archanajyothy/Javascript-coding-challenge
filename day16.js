648c5b16cb9a20d688cc270a26209662e37fcea9
archanajyothy <archanajyothy@gmail.com>
2023-06-06 10:24:07 +0530

    Javascript daily

---
+++ day16.js
@@ -0,0 +1,28 @@
+function anagram(s, t){
+    arr = []
+    for(let i=0;i<s.length;i++){
+        if (s.length !== t.length) {
+            console.log('false');
+            break;
+        }
+        for (let j = 0; j < t.length; j++) {
+            if(s[i] === t[j]){
+                arr.push(s[i])
+                t[j]='*'
+                break
+            }
+            
+        }
+    }
+    console.log(arr.join(''));
+    if(arr.join('') === s ){
+        console.log('true');
+    }else{
+        console.log('false');
+    }
+}
+s = 'anagram'
+t = 'nagaram'
+// s = 'rat'
+// t = 'cat'
+anagram(s,t)
