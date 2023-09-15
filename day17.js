11c6b5d881529b28782300dfa8633ca640f32132
archanajyothy <archanajyothy@gmail.com>
2023-09-16 00:00:37 +0530

    Day17 correct code

--- day17.js
+++ day17.js
@@ -2,7 +2,6 @@
 //Input - inputArray = ['abc', 'abc', 'abc']
 //Output - abc@abc@abc
 
-let inputArray = ['abc', 'abc', 'abc']
-newStr = inputArray.toString()
-newStr.split('@','abc')
-console.log(newStr)
+let inputArray = ['abc', 'abc', 'abc'];
+let newStr = inputArray.join('@'); 
+console.log(newStr);
