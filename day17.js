ad4163409303dc41c5a0dcb52789b518e307e3df
archanajyothy <archanajyothy@gmail.com>
2023-09-15 23:55:38 +0530

    day 17

---
+++ day17.js
@@ -0,0 +1,8 @@
+//Day17
+//Input - inputArray = ['abc', 'abc', 'abc']
+//Output - abc@abc@abc
+
+let inputArray = ['abc', 'abc', 'abc']
+newStr = inputArray.toString()
+newStr.split('@','abc')
+console.log(newStr)
