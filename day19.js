5aac3b476f3c4455a48022060157b708c5775bf8
archanajyothy <archanajyothy@gmail.com>
2023-09-18 23:57:21 +0530

    Explanation

--- day19.js
+++ day19.js
@@ -2,3 +2,6 @@
 // return the maximum number of consecutive 1's in the array
 //input : nums = [1,1,0,1,1,1]
 //output: 3
+/* Explanation: The first two digits or the 
+last three digits are consecutive 1s. 
+The maximum number of consecutive 1s is 3.*/ 
